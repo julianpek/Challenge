c6640316bd9aa3a84da2f1b20c7b9e5e55490a77
chefavocado <85806647+julianpek@users.noreply.github.com>
2022-06-25 17:17:29 -0400

    First Commit

---
+++ src/components/Customer.js
@@ -0,0 +1,22 @@
+const Customer = () => {
+  return (
+    <form>
+      <label>Name</label>
+      <input></input>
+      <label>Email</label>
+      <input></input>
+      <label>Channel</label>
+      <input></input>
+      <label>Address</label>
+      <input></input>
+      <label>Postal Code</label>
+      <input></input>
+      <label>City</label>
+      <input></input>
+      <label>Province</label>
+      <input></input>
+    </form>
+  );
+};
+
+export default Customer;
